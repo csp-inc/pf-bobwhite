5f770fc56fa6d0f2017e5d519fa10230f285b7ca
Patrick Freeman <patrick@csp-inc.org>
2023-04-24 08:13:07 -0700

    Create split-polygons-function.R

---
+++ code/utils/split-polygons-function.R
@@ -0,0 +1,20 @@
+library(sf)
+library(dismo)
+
+### SPlit the polygon into several approximately same area polygons 
+split_poly <- function(sf_poly, n_areas){
+  # create random points
+  points_rnd <- st_sample(sf_poly, size = 10000)
+  #k-means clustering
+  points <- do.call(rbind, st_geometry(points_rnd)) %>%
+    as_tibble() %>% setNames(c("lon","lat"))
+  k_means <- kmeans(points, centers = n_areas)
+  # create voronoi polygons
+  voronoi_polys <- dismo::voronoi(k_means$centers, ext = sf_poly)
+  # clip to sf_poly
+  crs(voronoi_polys) <- crs(sf_poly)
+  voronoi_sf <- st_as_sf(voronoi_polys)
+  equal_areas <- st_intersection(voronoi_sf, sf_poly)
+  equal_areas$area <- st_area(equal_areas)
+  return(equal_areas)
+}
